f58f4fc5a59da147f6a608771a27c016f93b8f90
Perry Mitchell <perry@perrymitchell.net>
2023-11-02 08:03:58 +0200

    Fix branch node encoding

--- source/BranchNode.ts
+++ source/BranchNode.ts
@@ -1,6 +1,6 @@
 import columnify from "columnify";
 import { Node } from "./Node.js";
-import { getIndent, indentString } from "./encoding.js";
+import { INDENT_SPACES, decodeMultilineEncoding, encodeMultilineColumn, getIndent, indentString } from "./encoding.js";
 
 export class BranchNode extends Node {
     protected _branchMeta: Record<string, any>;
@@ -15,12 +15,13 @@ export class BranchNode extends Node {
         if (Object.keys(this._branchMeta).length > 0) {
             const processedMeta = {};
             for (const key in this._branchMeta) {
-                processedMeta[key] = this._branchMeta[key];
+                processedMeta[key] = encodeMultilineColumn(JSON.stringify(this._branchMeta[key], undefined, INDENT_SPACES));
             }
-            const formatted = columnify(processedMeta, {
-                columnSplitter: " => ",
+            const formatted = decodeMultilineEncoding(columnify(processedMeta, {
+                columnSplitter: " | ",
+                preserveNewLines: true,
                 showHeaders: false
-            });
+            }));
             output = `${output}\n${indentString(formatted, indent + 1)}`;
         }
         return output;
